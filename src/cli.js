88db0168cb2aeffabab596125d43490a8ec95dc7
Rolf Erik Lekang <me@rolflekang.com>
2016-12-11 13:27:43 +0100

    fix: Add timezone on dates from ruter
    This should probably be moved out of cli

--- src/cli.js
+++ src/cli.js
@@ -10,19 +10,23 @@ const dot = chalk.bold(chalk.blue('·'));
 
 require('yargs')
   .usage('$0 <cmd> [args]')
-  .command('trip [from] [to]', 'Find a trip', ({ from, to }) => {
+  .command('trip [from] [to]', { from: {}, to: {} }, 'Find a trip', ({ from, to }) => {
     ruter.travel({ from, to })
       .then((response) => {
         console.log(
           `Travel from ${chalk.bold(response.from.Name)} to ${chalk.bold(response.to.Name)}`
         );
+
         response.trips.forEach((trip) => {
-          const departure = dateFns.format(new Date(trip.DepartureTime), 'HH:mm');
-          const arrival = dateFns.format(new Date(trip.ArrivalTime), 'HH:mm');
-          const diff = dateFns.differenceInMinutes(new Date(trip.ArrivalTime), new Date());
+          const departure = dateFns.format(new Date(`${trip.DepartureTime}+01:00`), 'HH:mm');
+          const arrival = dateFns.format(new Date(`${trip.ArrivalTime}+01:00`), 'HH:mm');
+          const diff = dateFns.differenceInMinutes(
+            new Date(`${trip.DepartureTime}+01:00`),
+            new Date()
+          );
           const output = `${dot} ${departure} - ${arrival} (${trip.TotalTravelTime})`;
           console.log(
-            diff < 5 ? output : chalk.dim(output)
+            diff < 5 ? chalk.dim(output) : output
           );
         });
       })
